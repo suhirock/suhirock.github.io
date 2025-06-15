c95ea0e186c367a05ad5d6909fc437c6955ccf12
suhirock <suhirock@users.noreply.github.com>
2025-06-15 07:50:29 +0000

    deploy: 8284d7ac5cabb205b2eab45ed99502331c333649

---
+++ _app/immutable/chunks/CYUdqag_.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_vn1lzy)==null?void 0:s.base)??"/suhirock.github.io";var t;const e=((t=globalThis.__sveltekit_vn1lzy)==null?void 0:t.assets)??a;export{e as a,a as b};
