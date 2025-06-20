548e25aab1ba2f69c88daa794801d4ec28df2736
suhirock <info@wai-ya.com>
2025-06-20 09:13:25 +0900

    apiちょっと修正

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,7 +1,7 @@
-import { json } from '@sveltejs/kit';
+import { json } from '@sveltejs/kit'
 import { getPosts } from '$lib/api/posts'
 
 export async function GET() {
-    const posts = await getPosts();
-    return json(posts);
+    const posts = await getPosts()
+    return json(posts)
 }
--- src/routes/api/posts/[year]/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from '@sveltejs/kit'
-import { getPosts } from '$lib/api/posts'
-
-export async function GET({ params }) {
-    const { year } = params
-    const posts = await getPosts(year)
-    return json(posts)
-}
